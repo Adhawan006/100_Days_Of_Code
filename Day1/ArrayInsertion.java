4aac1d15f279c8c70f98307985b0da20ce82d2f4
Adhawan006 <adityadhawan006@gmail.com>
2026-06-27 13:20:51 +0530

    Initial Commit

---
+++ Day1/ArrayInsertion.java
@@ -0,0 +1,47 @@
+//Insert an Element in an array
+
+package hundred_days_of_code.Day1;
+import java.util.Scanner;
+
+public class ArrayInsertion {
+    public static void main(String[] args) {
+     
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the length of array n: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Please enter " + n + " elements: ");
+        for(int x = 0; x < n; x++){
+            arr[x] = sc.nextInt();
+        }
+        System.out.println("Please enter the index of tobeInserted element: ");
+        int index = sc.nextInt();
+
+        System.out.println("Please enter the element to be inserted: ");
+        int newElement = sc.nextInt();
+        System.out.println("\n");
+
+        int[] newArr = new int[n+1];
+        
+       for (int x = 0; x < index; x++) {
+            newArr[x] = arr[x];
+        }
+
+        newArr[index] = newElement;
+
+
+        for (int x = index; x < n; x++) {
+           newArr[x + 1] = arr[x];
+    }
+
+       System.out.println("\nArray after insertion:");
+       for (int x = 0; x < newArr.length; x++) {
+         System.out.print(newArr[x] + " ");
+       }
+        sc.close();
+        
+    }
+
+}
---
+++ Day1/TwoSum.java
@@ -0,0 +1,30 @@
+package hundred_days_of_code.Day1;
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 26;
+
+        int[] ans = twoSum(nums, target);
+        System.out.println(ans[0] + " " + ans[1]);
+
+    }
+            
+    
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int x = 0; x<nums.length; x++){
+            int complement = target - nums[x];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), x};
+            }
+            map.put(nums[x], x);
+
+        }
+        return new int[]{-1, -1};
+        
+    }
+    }
+
