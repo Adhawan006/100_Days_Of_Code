aa551737560fc8da50c4fac4dc98bffb86408474
Adhawan006 <adityadhawan006@gmail.com>
2026-07-24 21:30:20 +0530

    Day 28: DSA Solutions

---
+++ Day28/Circular.java
@@ -0,0 +1,61 @@
+package hundred_days_of_code.Day28;
+
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Circular {
+
+    static Node createCircularLinkedList(int[] arr) {
+        if (arr.length == 0)
+            return null;
+
+        Node head = new Node(arr[0]);
+        Node temp = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            temp.next = new Node(arr[i]);
+            temp = temp.next;
+        }
+
+        // Make the list circular
+        temp.next = head;
+
+        return head;
+    }
+
+    static void traverseCircularLinkedList(Node head) {
+        if (head == null)
+            return;
+
+        Node temp = head;
+        do {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        } while (temp != head);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Node head = createCircularLinkedList(arr);
+        traverseCircularLinkedList(head);
+
+        sc.close();
+    }
+}
