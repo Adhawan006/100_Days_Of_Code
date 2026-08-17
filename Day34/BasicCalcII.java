29432eab2aef67c66ae1dee5e0c25f5821776cf9
Adhawan006 <adityadhawan006@gmail.com>
2026-08-17 09:41:23 +0530

    Day 35: DSA Solutions

---
+++ Day34/BasicCalcII.java
@@ -0,0 +1,44 @@
+package hundred_days_of_code.Day34;
+
+import java.util.Stack;
+
+class Solution {
+    public int calculate(String s) {
+        if (s == null || s.isEmpty()) return 0;
+        
+        Stack<Integer> stack = new Stack<>();
+        int currentNumber = 0;
+        char operation = '+';
+        
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            char currentChar = s.charAt(i);
+            
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            
+            if (!Character.isDigit(currentChar) && currentChar != ' ' || i == len - 1) {
+                if (operation == '+') {
+                    stack.push(currentNumber);
+                } else if (operation == '-') {
+                    stack.push(-currentNumber);
+                } else if (operation == '*') {
+                    stack.push(stack.pop() * currentNumber);
+                } else if (operation == '/') {
+                    stack.push(stack.pop() / currentNumber);
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        
+        return result;
+    }
+}
+
