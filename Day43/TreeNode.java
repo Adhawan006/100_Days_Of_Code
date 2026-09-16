32c910273556f49002bdd1761c56e589695bb4ab
Aditya Dhawan <adityadhawan006@gmail.com>
2026-09-16 20:14:16 +0530

    Day 43: DSA Solutions

---
+++ Day43/TreeNode.java
@@ -0,0 +1,34 @@
+package hundred_days_of_code.Day43;
+import java.util.*;
+
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        inorder(root, ans);
+        return ans;
+    }
+
+    void inorder(TreeNode node, List<Integer> ans) {
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left, ans);   
+        ans.add(node.val);         
+        inorder(node.right, ans);  
+    }
+}
+
+  public class TreeNode {
+      int val;
+     TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+          this.left = left;
+          this.right = right;    }
+  }
+ 
